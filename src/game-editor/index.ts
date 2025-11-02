eddc68879941c88fc03e6f6f513b816513a11837
Code Moreira <gabrielmoreirailustra@gmail.com>
2025-11-02 20:41:07 -0300

    chore: remove useless objectOptions

--- src/game-editor/index.ts
+++ src/game-editor/index.ts
@@ -13,37 +13,6 @@ import { splitObjectGroups } from "./engine/utils/split-object-groupds";
 import { Tile } from "./engine/manage-tiles";
 import { loadScreen } from "./engine/utils/loadScreen";
 
-const objectOptions = {
-  staticObjects: {},
-  dynamicObjects: {},
-  characters: {
-    peter: {
-      width: 30,
-      height: 50,
-      speed: 200,
-      texture: "rgba(22,103,212, 1)"
-    },
-    marty: {
-      width: 30,
-      height: 50,
-      speed: 200,
-      texture: "rgba(41,190,80, 1)"
-    },
-    steve: {
-      width: 30,
-      height: 50,
-      speed: 200,
-      texture: "rgba(212,118,22, 1)"
-    },
-    bruce: {
-      width: 30,
-      height: 50,
-      speed: 200,
-      texture: "rgba(200,50,150, 1)"
-    }
-  }
-};
-
 @customElement("game-editor")
 class GameEditor extends LitElement {
   static styles = [
